ee4848001ad789a6be619109f444d3968abc811c
gufronnakaaw <gufronnakaaw@gmail.com>
2023-07-31 12:22:00 +0700

    update: text and button in me page

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -37,7 +37,7 @@ function AboutMe() {
           <p className="font-recursive text-md mt-5 dark:text-gray-200">
             Currently learning about other <b>backend development</b> concepts
             such as <b>virtualization</b>, <b>containerization</b>,{' '}
-            <b>web servers</b>, etc to improve my skills.
+            <b>web servers</b>, <b>unit tests</b>, etc to improve my skills.
           </p>
         </motion.div>
         <motion.div
@@ -46,7 +46,7 @@ function AboutMe() {
           transition={{ duration: 0.7, delay: 0.8 }}
         >
           <p className="font-recursive text-md mt-5 dark:text-gray-200">
-            Outside working, I like to <b>travel</b> and <b>capture moments.</b>
+            Outside coding, I like to <b>travel</b> and <b>capture moments.</b>
           </p>
         </motion.div>
       </div>
@@ -57,8 +57,8 @@ function AboutMe() {
         transition={{ duration: 0.9, delay: 1 }}
       >
         <Link href="https://links.wildandev.com" target="_blank">
-          <button className="border-gray-100 hover:border-gray-200 transition border-2 rounded-lg font-bold hover:bg-gray-100 flex items-center p-4 font-outfit mt-5 dark:text-gray-200 dark:border-gray-500 dark:hover:border-gray-400 dark:hover:bg-gray-600">
-            Connect with me <FiArrowRight size="20"></FiArrowRight>
+          <button className="border-gray-400 transition border-2 rounded-lg font-bold hover:bg-gray-100 p-4 font-outfit mt-5 dark:text-gray-200 dark:hover:bg-gray-500">
+            Connect with me
           </button>
         </Link>
       </motion.div>
